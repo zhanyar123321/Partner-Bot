f95fa3d5a0fc1695afae32469e714536a7f577e4
zhanyar123321 <91457252+zhanyar123321@users.noreply.github.com>
2021-09-28 20:19:47 +0300

    Update Ano.js

--- Ano.js
+++ Ano.js
@@ -36,7 +36,7 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/AkYA68Z2gG`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/KTTmGRsszq`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
   client.channels.get("841792552314994709").send(
 `> <a:E14567BBEE164342AF58DF6872011807:853287588390633502> By: <@${message.author.id}> ✅ 
@@ -57,4 +57,4 @@ client.user.setGame(`PARTNER ANO`)
 
 
 
-client.login("");// Token Dane 
+client.login("ODkyNDU5ODkxMDA3MDQxNTU2.YVNOCw.EP4ZCS97yDofIdkFbxQDT1NPPNk");// Token Dane 
